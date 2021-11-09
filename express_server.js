50c7a3126cb79433db221fe8b79ce2ab9038c317
araff-16 <alex_raffa16@hotmail.com>
2021-11-09 00:26:25 +0000

    created function random string generator

--- express_server.js
+++ express_server.js
@@ -8,6 +8,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const generateRandomString = function() {
+  const options = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9'];
+  let string = '';
+  for (let i = 0; i < 6; i++) {
+    string += options[ Math.floor(Math.random() * (62))];
+  }
+  console.log(string);
+};
+
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -45,8 +55,8 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 
-
-
 app.listen(PORT, () =>{
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+generateRandomString();
